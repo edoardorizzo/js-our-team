f6bc3207ea81869d74db1c1c2c13dd2331bfe10d
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-22 16:27:27 +0100

    add all keys

--- assets/main.js
+++ assets/main.js
@@ -39,7 +39,7 @@ const team = [
 for (let index = 0; index < team.length; index++) {
     const singleMember = team[index];
     const memberList = document.createElement('li');
-    memberList.innerHTML = singleMember.role;
+    memberList.innerHTML = singleMember.name.role;
     ul.appendChild(memberList);
     console.log(singleMember);
     console.log(memberList);
